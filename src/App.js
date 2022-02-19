9006b37e3906075ed1d23f43186e835fea2f70c6
UlisesGallardo <0229262@up.edu.mx>
2022-02-18 23:49:44 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Registro from './components/Registro.js';
+
+function App() {
+  return (
+    <div>
+      <Registro/>
+    </div>
+  );
+}
+
+export default App;
